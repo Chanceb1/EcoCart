19de05d111bab67fb1cdf9bfb1d287c2bb64c906
Matthew_Cash <matthew@matthew-cash.com>
2025-01-22 15:31:36 -0800

    Fix type error when increasing item count in cart

--- frontend/src/components/Cart.tsx
+++ frontend/src/components/Cart.tsx
@@ -36,7 +36,7 @@ export default function Cart() {
                         name={item.name}
                         quantity={item.quantity}
                         price={item.price}
-                        onIncrease={() => cartCtx.addItem({item})}
+                        onIncrease={() => cartCtx.addItem(item)}
                         onDecrease={() => cartCtx.removeItem(item.id)}
                     />
                 ))}
