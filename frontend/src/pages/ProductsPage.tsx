3e86fdc83848e1a9f002fb3e3d42165e384c3a66
KleinMandolin <Samantha@Brewers.org>
2025-03-01 21:49:45 -0800

    Added rough sidebar to products

--- frontend/src/pages/ProductsPage.tsx
+++ frontend/src/pages/ProductsPage.tsx
@@ -1,12 +1,26 @@
 import ItemList from '../components/ItemList';
 import { CartContextProvider } from '../store/CartContext';
 import {UserProgressContextProvider} from '../store/UserProgressContext';
+import { AppSidebar } from "@/components/searchfilter-app-sidebar"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/searchfilter-sidebar"
 
 export default function ProductsPage() {
     return (
         <div className="container mx-auto py-8 align-middle text-center">
-            <h1 className="text-2xl font-bold mb-6">Our Products</h1>
-            <ItemList />
+            <SidebarProvider>
+                <AppSidebar />
+                <SidebarTrigger className="-ml-14 -mt-6" />
+                <SidebarInset className="ml-4">
+                    <h1 className="text-2xl font-bold mb-6">
+                        Our Products
+                    </h1>
+                    <ItemList/>
+                </SidebarInset>
+            </SidebarProvider>
         </div>
     );
 }
