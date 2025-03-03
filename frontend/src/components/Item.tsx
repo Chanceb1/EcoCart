de36eb92104f04c28a3643ee8f04e0fe3cfb6ef2
Matthew_Cash <matthew@matthew-cash.com>
2025-03-02 18:54:54 -0800

    Update demo products

--- frontend/src/components/Item.tsx
+++ frontend/src/components/Item.tsx
@@ -1,7 +1,7 @@
-import { JSX, useContext } from "react";
-import { currencyFormatter } from "../Utils/formatting";
-import { Button } from "./ui/Button";
-import { CartContext } from "../store/CartContext";
+import { JSX, useContext } from 'react';
+import { currencyFormatter } from '../Utils/formatting';
+import { Button } from './ui/Button';
+import { CartContext } from '../store/CartContext';
 
 export interface Item {
     id: string;
@@ -14,6 +14,7 @@ export interface Item {
 interface ItemProps {
     item: Item;
 }
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:5000';
 
 export default function Item({ item }: ItemProps): JSX.Element {
     const cartCtx = useContext(CartContext);
@@ -25,17 +26,29 @@ export default function Item({ item }: ItemProps): JSX.Element {
     return (
         <li className="bg-white rounded-lg shadow-lg overflow-hidden">
             <article className="bg-white rounded-lg overflow-hidden min-h-full">
-                <img src={`http://localhost:5000/${item.image}`} alt={item.name} className="w-full h-48 object-cover" />
+                <img
+                    src={`${apiBaseUrl}/${item.image}`}
+                    alt={item.name}
+                    className="w-full h-48 object-cover"
+                />
                 <div className="p-3">
                     <h3 className="text-lg font-semibold mb-2">{item.name}</h3>
                     <div className="grid grid-cols-2 gap-1 justify-center items-center">
-                        <p className="text-sm col-span-1 text-gray-700 text-right max-h-20 text-ellipsis overflow-clip">{item.description}</p>
+                        <p className="text-sm col-span-1 text-gray-700 text-right max-h-20 text-ellipsis overflow-clip">
+                            {item.description}
+                        </p>
                         <p className="text-2xl col-span-1 text-green-600 font-bold mb-2">
                             {currencyFormatter.format(item.price)}
                         </p>
                     </div>
                     <div className="p-4 text-center">
-                    <Button onClick={handleAddItemToCart} textOnly={true} className="add-to-cart-button">Add to Cart</Button>
+                        <Button
+                            onClick={handleAddItemToCart}
+                            textOnly={true}
+                            className="add-to-cart-button"
+                        >
+                            Add to Cart
+                        </Button>
                     </div>
                 </div>
             </article>
