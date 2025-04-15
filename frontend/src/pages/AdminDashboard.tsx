eae175b81bb296856630c273c9f78afd0a7074b6
BryanFrederickson <bryanfred03@gmail.com>
2025-04-15 14:44:41 -0700

    Switched view Analytics to view Transcations

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -53,13 +53,13 @@ const AdminDashboardPage = () => {
           <div className="bg-gray-100 dark:bg-gray-800 dark:text-white rounded-2xl shadow-md p-6 flex items-center">
           <img
             src={Analytics}
-            alt="View Analytics"
+            alt="View Transcations"
             className="w-24 h-24 rounded-lg mr-6 object-cover"
             />
             <div className="flex-1">
-              <h2 className="text-xl font-semibold text-green-700">View Analytics</h2>
+              <h2 className="text-xl font-semibold text-green-700">View Transcations</h2>
               <p className="text-sm text-gray-600 dark:text-gray-300">
-                View EcoCart platform performance and analytics.
+                View transcations made between Seller and Consumers.
               </p>
               <Button className="mt-4 bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700">
                 View
