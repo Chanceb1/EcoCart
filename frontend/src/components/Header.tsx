aeef7b56089a00a38a906cfe349cb779c5e332c6
Matthew_Cash <matthew@matthew-cash.com>
2025-04-15 21:21:40 -0700

    Hide account navbar link from logged out users

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,58 +1,16 @@
 import * as React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { cn } from '@/lib/utils.ts';
-// import { Icons } from '@/components/icons';
 import { useAuth } from '@/contexts/AuthContext.tsx';
 
 import {
     NavigationMenu,
-    NavigationMenuContent,
     NavigationMenuItem,
     NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    navigationMenuTriggerStyle
+    NavigationMenuList
 } from '@/components/ui/navigation-menu';
 
 import { ModeToggle } from './Theme/ModeToggle';
-
-const components: { title: string; href: string; description: string }[] = [
-    {
-        title: 'Alert Dialog',
-        href: '/docs/primitives/alert-dialog',
-        description:
-            'A modal dialog that interrupts the user with important content and expects a response.'
-    },
-    {
-        title: 'Hover Card',
-        href: '/docs/primitives/hover-card',
-        description:
-            'For sighted users to preview content available behind a link.'
-    },
-    {
-        title: 'Progress',
-        href: '/docs/primitives/progress',
-        description:
-            'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.'
-    },
-    {
-        title: 'Scroll-area',
-        href: '/docs/primitives/scroll-area',
-        description: 'Visually or semantically separates content.'
-    },
-    {
-        title: 'Tabs',
-        href: '/docs/primitives/tabs',
-        description:
-            'A set of layered sections of content—known as tab panels—that are displayed one at a time.'
-    },
-    {
-        title: 'Tooltip',
-        href: '/docs/primitives/tooltip',
-        description:
-            'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.'
-    }
-];
 
 export default function Header() {
     const { isAuthenticated, logout, user } = useAuth();
@@ -128,12 +86,14 @@ export default function Header() {
                 </NavigationMenu>
 
                 <div className="flex items-center gap-2">
-                    <Link
-                        to="/account"
-                        className={transparentNavigationMenuTriggerStyle()}
-                    >
-                        Account
-                    </Link>
+                    {isAuthenticated && (
+                        <Link
+                            to="/account"
+                            className={transparentNavigationMenuTriggerStyle()}
+                        >
+                            Account
+                        </Link>
+                    )}
                     <ModeToggle />
                     <Link
                         to="/cart"
